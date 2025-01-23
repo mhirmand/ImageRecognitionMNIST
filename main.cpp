3921fe127cee02a98f77cdc5205f6cb447f461ca
Reza Hirmand <mohammadreza.hirmand@hexagon.com>
2025-01-23 14:23:19 -0500

    Clean up

--- main.cpp
+++ main.cpp
@@ -30,20 +30,6 @@ int main() {
         new SigmoidLayer()
     });
 
-  //// Initialize CNN with 10 layers
-  //CNN cnn({
-  //  new ConvLayer(1, 32, 3, 1, 28, 28),  // Input: 28x28x1, Output: 26x26x32
-  //  new ReLULayer(),
-  //  new MaxPoolLayer(2, 2, 26, 26, 32),  // Output: 13x13x32
-  //  new ConvLayer(32, 64, 3, 1, 13, 13), // Output: 11x11x64
-  //  new ReLULayer(),
-  //  new MaxPoolLayer(2, 2, 11, 11, 64),  // Output: 5x5x64
-  //  new FCLayer(5 * 5 * 64, 128),
-  //  new ReLULayer(),
-  //  new FCLayer(128, 10),
-  //  new SigmoidLayer()
-  //  });
-
   // Training parameters
   int epochs = 5;
   int batch_size = 1000;
